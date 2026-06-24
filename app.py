69e56938402e9dfaba7bbfb6af348a6b7631e8d4
Nikhilesh357 <68781674+Nikhilesh357@users.noreply.github.com>
2026-06-24 18:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ZMA8Ebod8JfoTM3PqjDL2hOP0TkuDcDt
 """
 
-pip install streamlit
-
-import streamlit as st
-import pandas as pd
-import joblib
 
 rf = joblib.load('salary_predictor.pkl')
 title_encoder = joblib.load('title_encoder.pkl')
@@ -21,8 +16,6 @@ state_encoder = joblib.load('state_encoder.pkl')
 county_encoder = joblib.load('county_encoder.pkl')
 
 df = pd.read_csv('data/jobs.csv')
-
-import streamlit as st
 
 title_dropdown = st.selectbox(
     options=sorted(title_encoder.classes_),
